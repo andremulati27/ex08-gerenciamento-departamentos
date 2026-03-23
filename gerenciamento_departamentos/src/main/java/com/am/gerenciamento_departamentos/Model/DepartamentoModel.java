e9a86ad76e1dcdc3721e7969bb81fb9789dcc163
Andre <andremulati@gmail.com>
2026-03-23 17:59:43 -0300

    criando model

---
+++ gerenciamento_departamentos/src/main/java/com/am/gerenciamento_departamentos/Model/DepartamentoModel.java
@@ -0,0 +1,29 @@
+package com.am.gerenciamento_departamentos.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_departamento")
+public class DepartamentoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nomeDepartamento")
+    private String nome;
+    @Column(name = "localizacaoDepartamento")
+    private String localizacao;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome;}
+
+    public String getLocalizacao() { return localizacao; }
+
+    public void setLocalizacao(String localizacao) { this.localizacao = localizacao; }
+}
+
